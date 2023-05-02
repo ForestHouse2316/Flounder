a9e02b4b1d6cae59622a5b12d6fa837c3a4d7917
ForestHouse <leo2316park@gmail.com>
2023-05-03 02:15:45 +0900

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mermaid-flounder',
+      version='1.0.0a1',
+      description='Automation tools for Mermaid diagram',
+      author='ForestHouse',
+      author_email='foresthouse2316@gmail.com',
+      url='https://github.com/ForestHouse2316/flounder',
+      license='MIT',
+      py_modules=['flounder'],
+      python_requires='>=3',
+      install_requires=[],
+      packages=['flounder']
+      )
